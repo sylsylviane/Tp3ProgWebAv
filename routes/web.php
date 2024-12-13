802d56ee043a9999fb1c1c0496050c9a4c23c881
sylsylviane <syl.sylviane1@gmail.com>
2024-12-13 10:31:25 -0500

    Ajouter les routes login/logout

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('/realisateur', 'RealisateurController@index');
 Route::get('user/create', 'UserController@create');
 Route::post('user/create', 'UserController@store');
 
+Route::get('/login', 'AuthController@index');
+Route::post('/login', 'AuthController@store');
+Route::get('/logout', 'AuthController@delete');
+
 Route::dispatch();
 
 ?>
