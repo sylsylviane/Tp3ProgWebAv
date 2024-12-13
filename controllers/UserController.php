320c3a42e80c0d4660e34c29ea36cecd08af93cd
sylsylviane <syl.sylviane1@gmail.com>
2024-12-13 09:53:58 -0500

    Créer fonction create et store dans la classe UserController

---
+++ controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\User;
+use App\Models\Privilege;
+use App\Providers\View;
+use App\Providers\Validator;
+
+class UserController{
+    /**
+     *  Récupère les données de privilèges de la base de données et retourne le modèle de view user/create
+     */
+    public function create(){
+        $privilege = new Privilege;
+        $privileges = $privilege->select('privilege');
+        return View::render('user/create', ['privileges'=>$privileges]);
+    }
+
+    public function store($data = []){
+        $validator = new Validator;
+        $validator->field('name', $data['name'])->min(2)->max(50);
+         $validator->field('username', $data['username'])->unique('User')->email()->min(2)->max(50);
+        $validator->field('password', $data['password'])->min(6)->max(20);
+        $validator->field('email', $data['email'])->required()->email()->max(100);
+        $validator->field('privile_id', $data['privilege_id'])->required()->int();
+
+        if ($validator->isSuccess()){
+            $user = new User;
+            $data['password'] = $user->hashPassword($data['password']);
+            $insert = $user->insert($data);
+
+            if($insert){
+                return View::redirect('login');
+            }else{
+                return View::render('error');
+            }
+        }else{
+            $errors = $validator->getErrors();
+            $privilege = new Privilege;
+            $privileges = $privilege->select('privilege');
+            return View::render('user/create', ['errors'=>$errors, 'user'=>$data, 'privileges'=>$privileges]);
+        }
+    }
+}
+
