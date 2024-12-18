8ad4505603ba93593f823f922a54c2b1310f44a8
sylsylviane <syl.sylviane1@gmail.com>
2024-12-18 10:31:55 -0500

    Modifier classe View

--- providers/View.php
+++ providers/View.php
@@ -1,22 +1,26 @@
 <?php
-
 namespace App\Providers;
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-class View
-{
-    static public function render($template, $data = [])
-    {
+class View {
+    static public function render($template, $data=[]){
         $loader = new FilesystemLoader('views');
         $twig = new Environment($loader);
         $twig->addGlobal('asset', ASSET);
         $twig->addGlobal('base', BASE);
-        echo $twig->render($template . '.php', $data);
+        if(isset($_SESSION['finger_print']) and $_SESSION['finger_print']===md5($_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR'])){
+            $guest = false;
+        }else{
+            $guest = true;
+        }
+        $twig->addGlobal('guest', $guest);
+        $twig->addGlobal('session', $_SESSION);
+        echo $twig->render($template.".php", $data);
     }
-    static public function redirect($url)
-    {
-        header('location:' . BASE . '/' . $url);
+
+    static public function redirect($url){
+        header('location:'.BASE.'/'.$url);
     }
 }
