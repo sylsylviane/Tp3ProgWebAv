794ea6051c90a2439dda9abe5773a50a723fc2ca
sylsylviane <syl.sylviane1@gmail.com>
2024-12-18 10:17:42 -0500

    Ajouter condition invité ou user

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+
+session_start();
 require_once 'config.php';
 require_once 'vendor/autoload.php';
 require_once 'routes/web.php';
+
+use App\Models\Journal;
+
+$journal = new Journal;
+$ip = $_SERVER['REMOTE_ADDR'];
+if (isset($_SESSION['user_name'])) {
+    $username = $_SESSION['user_name'];
+} else {
+    $username = 'Invité';
+}
+$page_visitee = $_SERVER['PHP_SELF'];
+
+$insert = $journal->insert(['adresse_ip' => $ip, 'nom_utilisateur' => $username, 'page_visitee' => $page_visitee]);
+
+
+
