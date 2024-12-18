320a3bd3e130188d9d487b12dbee89db8a0ea55f
sylsylviane <syl.sylviane1@gmail.com>
2024-12-18 10:23:41 -0500

    Ajouter constructeur et formatter fichier

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,48 +1,52 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Models\User;
 use App\Models\Privilege;
 use App\Providers\View;
 use App\Providers\Validator;
+use App\Providers\Auth;
 
-class UserController{
-    /**
-     *  Récupère les données de privilèges de la base de données et retourne le modèle de view user/create
-     */
-    public function create(){
-        $privilege = new Privilege;
-        $privileges = $privilege->select('privilege');
-        return View::render('user/create', ['privileges'=>$privileges]);
+class UserController
+{
+
+    public function __construct()
+    {
+        Auth::session();
+        Auth::privilege(1);
     }
-    
-    /**
-     * Valide les donneés saisies par l'utilisateur et les insère dans la base de données si la validation est réussit. Sinon, gère les erreurs. 
-     */
-    public function store($data = []){
+
+    public function create()
+    {
+        $privilege = new Privilege;
+        $privileges = $privilege->select();
+        return View::render('user/create', ['privileges' => $privileges]);
+    }
+
+    public function store($data)
+    {
         $validator = new Validator;
-        $validator->field('name', $data['name'])->min(2)->max(50);
-         $validator->field('username', $data['username'])->unique('User')->email()->min(2)->max(50);
-        $validator->field('password', $data['password'])->min(6)->max(20);
-        $validator->field('email', $data['email'])->required()->email()->max(100);
-        $validator->field('privile_id', $data['privilege_id'])->required()->int();
+        $validator->field('name', $data['name'], 'Le nom')->min(2)->max(50);
+        $validator->field('username', $data['username'], 'Le nom d\'utilisateur')->min(2)->max(50)->unique('User');
+        $validator->field('password', $data['password'], 'Le mot de passe')->min(6)->max(20);
+        $validator->field('privilege_id', $data['privilege_id'], "Le privilege")->required()->number();
 
-        if ($validator->isSuccess()){
+        if ($validator->isSuccess()) {
             $user = new User;
+            $data['email'] = $data['username'];
             $data['password'] = $user->hashPassword($data['password']);
-            $insert = $user->insert($data);
-
-            if($insert){
+            $insert  = $user->insert($data);
+            if ($insert) {
                 return View::redirect('login');
-            }else{
+            } else {
                 return View::render('error');
             }
-        }else{
+        } else {
             $errors = $validator->getErrors();
             $privilege = new Privilege;
-            $privileges = $privilege->select('privilege');
-            return View::render('user/create', ['errors'=>$errors, 'user'=>$data, 'privileges'=>$privileges]);
+            $privileges = $privilege->select();
+            return View::render('user/create', ['errors' => $errors, 'inputs' => $data, 'privileges' => $privileges]);
         }
     }
 }
-
